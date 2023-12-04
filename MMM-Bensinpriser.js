fb19b83b3d367e0bb96da89fa83f1320c3fc7d0b
ItsMeBrille <herhellum@gmail.com>
2023-12-04 14:02:13 +0100

    added last update station text

--- MMM-Bensinpriser.js
+++ MMM-Bensinpriser.js
@@ -13,7 +13,7 @@ Module.register("MMM-Bensinpriser", {
 
   },
 
-  start: function() {
+  start: function () {
     this.wrapper = document.createElement("div");
     this.wrapper.className = "fuel-price-wrapper";
     this.content = document.createElement("div");
@@ -25,16 +25,16 @@ Module.register("MMM-Bensinpriser", {
     this.scheduleUpdate();
   },
 
-  scheduleUpdate: function() {
+  scheduleUpdate: function () {
     const self = this;
-    setInterval(function() {
+    setInterval(function () {
       self.loadData();
-    }, this.config.updateInterval*60000);
+    }, this.config.updateInterval * 60000);
   },
 
   // ... other functions ...
 
-  loadData: function() {
+  loadData: function () {
     const url = `${this.config.apiURL}`;
     const headers = {
       "X-API-KEY": this.config.apiKey,
@@ -61,77 +61,94 @@ Module.register("MMM-Bensinpriser", {
   },
 
 
-  sortStationsByDistance: function(stations) {
+  sortStationsByDistance: function (stations) {
     const { latitude, longitude } = this.config.coordinates;
-  
+
     stations.forEach(station => {
       const stationLatitude = parseFloat(station.latitude);
       const stationLongitude = parseFloat(station.longitude);
-  
+
       const distance = this.calculateDistance(latitude, longitude, stationLatitude, stationLongitude);
       station.distance = distance;
     });
-  
+
     stations.sort((a, b) => a.distance - b.distance);
-  
+
     return stations;
   },
-  
-  calculateDistance: function(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Earth's radius in kilometers
+
+  deg2rad: function (deg) {
+    return deg * (Math.PI / 180);
+  },
+
+  // Calculating distance between two points on a globe
+  calculateDistance: function (lat1, lon1, lat2, lon2) {
+    const R = 6371;
     const dLat = this.deg2rad(lat2 - lat1);
     const dLon = this.deg2rad(lon2 - lon1);
     const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
+    const distance = R * c;
 
     return distance;
   },
 
-  deg2rad: function(deg) {
-    return deg * (Math.PI / 180);
-  },
-
-  createTable: function(stations) {
+  createTable: function (stations) {
     const self = this;
     const table = document.createElement("table");
     table.className = "fuel-price-table";
-  
+
     const headerRow = document.createElement("tr");
     headerRow.innerHTML = `<th></th><th>${self.translate("stationHeader")}</th>${this.config.fuelTypes.map(type => `<th>${type}</th>`).join("")}`;
     table.appendChild(headerRow);
-  
+
+    updates = [];
     for (const station of stations) {
       const stationRow = document.createElement("tr");
       const stationName = station.name + (station.extras?.facilities?.airPump ? " &#129520;" : ""); // Add marker if there are airpumps at the given station
       stationRow.innerHTML = `<td><img class="station-icon" src="${station.pictureUrl}"></td><td>${stationName}</td>${this.config.fuelTypes.map(type => this.getPriceCell(station, type)).join("")}`;
       table.appendChild(stationRow);
+
+      updates.push(new Date(station.lastUpdated));
     }
-  
+
     const wrapper = document.createElement("div");
     wrapper.appendChild(table);
-  
+    const lastUpdate = document.createElement("p");
+    lastUpdate.className = "fuel-last-updated";
+
+
+    const seconds = Math.floor((new Date() - new Date(Math.min.apply(null, updates))));
+    // If time is more than 30 minutes, show hours
+    if (seconds > 1800000) {
+      lastUpdate.innerHTML = `Sist oppdatert: ${Math.round(seconds / 3600000)} ${seconds / 3600000 > 1 ? "time" : "timer"} siden`;
+    } else {
+      lastUpdate.innerHTML = `Oppdatert nå nettopp`;
+    }
+
+    wrapper.appendChild(lastUpdate);
+
     return wrapper;
   },
-  
-  getPriceCell: function(station, type) {
+
+  getPriceCell: function (station, type) {
     const fuelTypeDetails = station.stationDetails.find(detail => detail.type === type);
     const decimalSeparator = this.config.decimalSeparator; // Get the configured decimal separator
     const priceFormatted = fuelTypeDetails ? `${fuelTypeDetails.price.toFixed(2).replace(".", decimalSeparator)} kr` : "";
     return `<td>${priceFormatted}</td>`;
   },
-  
 
-  loadLanguageJSON: function(language) {
+
+  loadLanguageJSON: function (language) {
     const self = this;
     const file = this.file(`translations/${language}.json`);
     console.log(file);
     const xhr = new XMLHttpRequest();
     xhr.overrideMimeType("application/json");
     xhr.open("GET", file, true);
-    xhr.onreadystatechange = function(){
+    xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         self.translateJSON = JSON.parse(xhr.responseText);
       }
@@ -139,18 +156,18 @@ Module.register("MMM-Bensinpriser", {
     xhr.send(null);
   },
 
-  translate: function(key) {
+  translate: function (key) {
     if (this.translateJSON && this.translateJSON[key]) {
       return this.translateJSON[key];
     }
     return key;
   },
 
-  getStyles: function() {
+  getStyles: function () {
     return ["MMM-Bensinpriser.css"]; // Add your CSS file
   },
 
-  getDom: function() {
+  getDom: function () {
     return this.wrapper;
   },
 
